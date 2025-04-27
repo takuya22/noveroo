3cc7a97dfb2b084d5d2a68e55358eece7bbcb595
takuya22 <taku22yana2020@gmail.com>
2025-04-27 12:26:35 +0900

    fix eror

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,32 @@
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, token }) {
+      if (session.user) {
+        session.user.id = token.sub || user.id;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
